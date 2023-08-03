5189cabc14b2298883c59c6054080ff72abcccd3
Joshua Giardini <joshua.g@visiaquantum.com>
2023-08-03 16:22:57 +0200

    FIX | common removed

--- floor/lib/src/sqflite_database_factory.dart
+++ floor/lib/src/sqflite_database_factory.dart
@@ -1,15 +1,12 @@
 import 'dart:io';
 
 import 'package:path/path.dart';
-import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite_sqlcipher/sqflite.dart';
 
 // infers factory as nullable without explicit type definition
 final DatabaseFactory sqfliteDatabaseFactory = () {
   if (Platform.isAndroid || Platform.isIOS) {
     return databaseFactory;
-  } else if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
-    sqfliteFfiInit();
-    return databaseFactoryFfi;
   } else {
     throw UnsupportedError(
       'Platform ${Platform.operatingSystem} is not supported by Floor.',
